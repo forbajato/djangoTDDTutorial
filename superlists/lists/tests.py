765bc4d2028d847e8938c8bb919fc62d28b47ab2
Thomas C Hicks <paradox@pobox.com>
2014-12-24 12:03:01 +0800

    Each list should have its own url - getting wrong error code here

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,16 +7,36 @@ from lists.views import home_page
 from lists.models import Item, List
 
 class ListViewTest(TestCase):
-    
-    def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
+    
+    #def test_displays_all_items(self):
+        #list_ = List.objects.create()
+        #Item.objects.create(text='itemey 1', list=list_)
+        #Item.objects.create(text='itemey 2', list=list_)
+
+        #response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        #self.assertContains(response, 'itemey 1')
+        #self.assertContains(response, 'itemey 2')
 
 class ListAndItemModelsTest(TestCase):
     
@@ -59,9 +79,9 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(),expected_html)
         
-    def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertTemplateUsed(response, 'list.html')
+    #def test_uses_list_template(self):
+        #response = self.client.get('/lists/the-only-list-in-the-world/')
+        #self.assertTemplateUsed(response, 'list.html')
 
 class NewListTest(TestCase):
 
@@ -80,6 +100,5 @@ class NewListTest(TestCase):
                 '/lists/new',
                 data = {'item_text': 'A new list item'}
                 )
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
-        #self.assertEqual(response.status_code, 302)
-        #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
